6b588a6cbcab1b12abb30a09e38b1aaadecdac37
JoJoPotatos <45069753+JoJoPotatos@users.noreply.github.com>
2018-11-29 22:54:58 -0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Good Morning! I'm learning C++!\n";
+	cout << "Click ENTER to exit test application...\n";
+	cin.get();
+	return 0;
+}
